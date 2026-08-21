50cb184d8b3d76d8e70d8aa41b6cf059ba97f3e6
Adhithyanvenkat <adhivenkat46@gmail.com>
2026-08-21 14:49:01 +0530

    Time: 574 ms (24.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select customer_number as customer_number from (select customer_number,count(customer_number) as a from Orders group by customer_number order by a desc limit 1 )  t ;
